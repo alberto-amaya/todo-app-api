a22a35d2399c6bc1aad18f6a48fc2937e1ebd1ae
Angel Arceo <aalberto1812@gmail.com>
2022-04-25 21:57:59 -0500

    adding a test route

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
 })
 
+app.get('/api/todos', (request, response) => {
+  response.json({
+    title: 'What?'
+  })
+})
+
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
